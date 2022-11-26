cf067d604d71483a20e7ad5ec9464bac5a4337a4
Akash <akash7890123@gmail.com>
2022-11-26 14:01:21 +0530

    changed settings to run correctly in production env

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
-import basicSsl from '@vitejs/plugin-basic-ssl'
 import react from '@vitejs/plugin-react'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+
+let plugins = []
+
+if(!process.env.PROD){
+  plugins.push(basicSsl())
+}
+
+plugins.push(react())
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [basicSsl(), react()]
+  plugins
 })
