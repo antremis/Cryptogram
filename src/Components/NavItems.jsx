8fb0d9084e9bd06dae5062a875f573cc8986f464
Akash <akash7890123@gmail.com>
2022-12-19 15:29:22 +0530

    Changed default error message

--- src/Components/NavItems.jsx
+++ src/Components/NavItems.jsx
@@ -4,7 +4,7 @@ import {notify} from './Alert'
 const NavItems = ({src, ref, action}) => {
 
     return(
-        <img src={src} data-nav-item onClick={action?action:()=>notify({alert:src, type:'error'})} />
+        <img src={src} data-nav-item onClick={action?action:()=>notify({alert:'Not implemented yet', type:'error'})} />
     )
 }
 
